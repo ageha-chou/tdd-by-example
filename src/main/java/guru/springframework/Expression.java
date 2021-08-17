287d34e6e3194f7a2b1121d2ded40ad6c4d6aa22
ageha-chou <agehachou2602@gmail.com>
2021-08-18 00:31:32 +0700

    mixed currency

--- src/main/java/guru/springframework/Expression.java
+++ src/main/java/guru/springframework/Expression.java
@@ -1,5 +1,7 @@
 package guru.springframework;
 
 public interface Expression {
+    Expression plus(Expression addend);
+
     Money reduce(Bank bank, String to);
 }
--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -14,7 +14,7 @@ public class Money implements Expression {
         return currency;
     }
 
-    public Money times (int multiplier) {
+    public Expression times (int multiplier) {
         return new Money(this.amount * multiplier, this.currency);
     }
 
@@ -26,7 +26,8 @@ public class Money implements Expression {
         return new Money(amount, "CHF");
     }
 
-    public Expression plus(Money addend) {
+    @Override
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
