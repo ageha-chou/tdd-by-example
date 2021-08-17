86ad6493c84a9301d9b22c668a3455c3177d1f07
ageha-chou <agehachou2602@gmail.com>
2021-08-17 22:54:15 +0700

    from start to additional finally

---
+++ src/main/java/guru/springframework/Money.java
@@ -0,0 +1,48 @@
+package guru.springframework;
+
+
+public class Money implements Expression {
+    protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    protected String currency() {
+        return currency;
+    }
+
+    public Money times (int multiplier) {
+        return new Money(this.amount * multiplier, this.currency);
+    }
+
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    public Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
+    }
+
+    public boolean equals(Object obj) {
+        Money money = (Money) obj;
+        return this.amount == money.amount
+                && this.currency == money.currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+
+}
